a9b5f40d2cdfc74994883d7af6fd15e042faa4c8
Avoyken <prey-88@yandex.ru>
2020-10-15 19:16:36 +0300

    init

---
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="footer" class="footer">
+    <p v-text="$t('footer')">This is your own footer</p>
+  </div>
+</template>
+
+<script lang="ts" src="./jhi-footer.component.ts"></script>
